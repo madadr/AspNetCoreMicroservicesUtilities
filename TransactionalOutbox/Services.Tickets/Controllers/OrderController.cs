98ddfa87025ebb10bc3f98e65d3ce0c8c6b0827c
madadr <madadr@outlook.com>
2020-05-13 23:06:53 +0200

    Initialized TransactionalOutbox

---
+++ TransactionalOutbox/Services.Tickets/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Services.Tickets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger) => _logger = logger;
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string seat)
+        {
+            _logger.LogInformation($"Ticket order for seat {seat} requested.");
+            return !string.IsNullOrEmpty(seat) ? (IActionResult) Accepted() : BadRequest();
+        }
+    }
+}
